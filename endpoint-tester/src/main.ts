9d84c84d20d8b5b1ecddaaa9b48a860fc47f9e6d
Jimmy Chu <jimmychu0807@gmail.com>
2024-02-24 17:14:29 +0800

    updated

--- endpoint-tester/src/main.ts
+++ endpoint-tester/src/main.ts
@@ -1,9 +1,23 @@
-import { load } from "$std/dotenv/mod.ts";
+import { load } from '$std/dotenv/mod.ts';
+import { parse } from '$std/jsonc/mod.ts';
 import { ApiPromise, WsProvider } from '$polkadot-js/api/mod.ts';
 
-const env = await load();
+const config = parse(await Deno.readTextFile('./src/config.jsonc'));
 
-const provider = new WsProvider(env.RPC_ENDPOINT);
-const api = await ApiPromise.create({ provider });
+const { endPoints, connections, txs } = config;
 
-console.log(`hash: ${api.genesisHash.toHex()}`);
+const connArr = endPoints.reduce(
+	(memo, ep) => memo.concat([...Array(connections).keys()].map(() => ep)),
+	[],
+);
+
+const apiPromises = connArr.map((ep) => ApiPromise.create({ provider: new WsProvider(ep) }));
+const apis = await Promise.allSettled(apiPromises);
+
+// Issuing the same requests
+apis.forEach((api) => {
+	// NS> formuate a tx from a txt script to executable toward an endpoints.
+	// NS> you can also time the txs when the result is included with tx receipts.
+});
+
+Deno.exit();
