1c7c9f8f5b700298d75791bfbca2481814f7fc94
Jimmy Chu <jimmychu0807@gmail.com>
2024-02-27 12:51:38 +0800

    Can retrieve dev account properly

--- endpoint-tester/src/main.ts
+++ endpoint-tester/src/main.ts
@@ -1,23 +1,72 @@
 import { parse } from "$std/jsonc/mod.ts";
 import { ApiPromise, WsProvider } from "$polkadot-js/api/mod.ts";
+import { Keyring } from "$polkadot-js/keyring/mod.ts";
+import * as bnjs from "$bn.js";
+
+const APP_CONFIG = "./src/config.jsonc";
+const DEV_SEED_PHRASE = "bottom drive obey lake curtain smoke basket hold race lonely fit walk";
+const DEV_ACCTS = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Fredie"];
+const API_PREFIX = "api";
+const BN = bnjs.BN;
+
+const config = parse(await Deno.readTextFile(APP_CONFIG));
+const keyring = new Keyring(config.keyring);
+
+function getTxCall(api: ApiPromise, txStr: string): Function {
+  const segs = txStr.split(".");
+  return segs.reduce(
+    (txCall, seg, idx) => idx === 0 && seg === API_PREFIX ? txCall : txCall[seg],
+    api
+  );
+}
+
+function isWriteOp(txStr: string): boolean {
+  return txStr.includes("tx.");
+}
+
+function transformParams(params: Array<string>) {
+  return params.map(param => {
+    if (DEV_ACCTS.includes(param)) {
+      const acct = keyring.addFromUri(`${DEV_SEED_PHRASE}//${param}`);
+      return acct.address;
+    }
+    return param;
+  });
+}
+
+function transformResult(result) {
+  return result.toJSON();
+}
 
 async function sendTxsToApi(api: ApiPromise, txs) {
+  let txStr;
   let lastResult;
-  for (const txStr of txs) {
-    if (typeof txStr === "string") {
-      const segs = txStr.split(".");
-      const txObj = segs.reduce((txObj, seg) => txObj[seg], api);
-      lastResult = await txObj.call();
+  for (const tx of txs) {
+    if (typeof tx === "string") {
+      txStr = tx;
+      const txCall = getTxCall(api, tx);
+      lastResult = await txCall.call(txCall);
+    } else {
+      const { params, sign } = tx;
+      txStr = tx.tx;
+      const txCall = getTxCall(api, txStr);
+      const transformedParams = transformParams(params);
+
+      if (isWriteOp(txStr)) {
+        // lastResult is block hash. It hasn't been put in block or finalized yet
+        lastResult = await txCall.call(txCall, ...transformedParams).signAndSend(sign);
+      } else {
+        lastResult = await txCall.call(txCall, ...transformedParams);
+      }
     }
+    lastResult = transformResult(lastResult);
+    console.log(`${txStr}\n  L`, lastResult);
   }
   return lastResult;
 }
 
 async function main() {
-  const config = parse(await Deno.readTextFile("./src/config.jsonc"));
-
   const { endPoints, connections, txs } = config;
-
   const connArr = endPoints.reduce(
     (memo, ep) => memo.concat([...Array(connections).keys()].map(() => ep)),
     [],
@@ -35,8 +84,6 @@ async function main() {
   results = await Promise.allSettled(
     apis.map((api) => sendTxsToApi(api, txs)),
   );
-
-  console.log(`results:`, results);
 }
 
 main()
