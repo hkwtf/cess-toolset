33ac1f834a0d2302e3bfea7ec35becd19aa7be9c
Jimmy Chu <jimmychu0807@gmail.com>
2024-03-14 15:27:46 +0800

    updated with config schema checking

--- substrate-rpc-tester/src/configSchema.ts
+++ substrate-rpc-tester/src/configSchema.ts
@@ -1,7 +1,9 @@
 import Ajv from "ajv/dist/jtd.js";
+import type { JsonValue } from "std/json/common.ts";
+// our own implementation
+import type { AppConfig } from "./types.ts";
 
 const ajv = new Ajv.default({ allErrors: true });
-
 const configSchema = {
   properties: {
     endPoint: { type: "string" },
@@ -29,7 +31,45 @@ const configSchema = {
   },
 };
 
-const validate = ajv.compile(configSchema);
+const txObjSchema = {
+  properties: {
+    tx: { type: "string" },
+  },
+  optionalProperties: {
+    signer: { type: "string" },
+    params: {
+      elements: {},
+    },
+  },
+};
+
+const overallValidate = ajv.compile(configSchema);
+const txValidate = ajv.compile(txObjSchema);
+
+type ValidateFunc = {
+  (config: JsonValue): boolean;
+  errors?: Array<unknown> | null;
+};
+
+const validate: ValidateFunc = (config: JsonValue) => {
+  if (!overallValidate(config)) {
+    validate.errors = overallValidate.errors;
+    return false;
+  }
+
+  if (config && Object.hasOwn(config as object, "txs")) {
+    const txs = (config as unknown as AppConfig).txs;
+    for (const tx of txs) {
+      if (typeof tx === "string") continue;
+      if (!txValidate(tx)) {
+        validate.errors = txValidate.errors;
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
 
 export default validate;
 export { configSchema };
